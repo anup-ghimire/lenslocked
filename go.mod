ee77c5147d4c80d85b63504c1b3a220d81cf1027
anup-ghimire <anupghimire96@gmail.com>
2022-10-16 21:10:26 -0500

    Working with Chi

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/anup-ghimire/lenslocked
 
 go 1.15
 
-require github.com/go-chi/chi/v5 v5.0.7 // indirect
+require github.com/go-chi/chi/v5 v5.0.7
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/go-chi/chi/v5"
 	"net/http"
 )
 
@@ -32,71 +33,17 @@ func faqHandler(w http.ResponseWriter, r *http.Request){
 		`)
 }
 
-
-//func pathHandler(w http.ResponseWriter, r *http.Request){
-//	switch r.URL.Path{
-//	case "/":
-//		homeHandler(w,r)
-//	case "/contact":
-//		contactHandler(w,r)
-//	case "/faqs":
-//		faqHandler(w, r)
-//	default:
-//		http.Error(w, "Page not found.", http.StatusNotFound)
-//		//w.WriteHeader(http.StatusNotFound)
-//		//fmt.Fprintf(w, "<p>Page not found.")
-//	}
-//}
-
-type Router struct{}
-
-func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/":
-		homeHandler(w, r)
-	case "/contact":
-		contactHandler(w, r)
-	case "/faq":
-		faqHandler(w, r)
-	default:
-		http.Error(w, "Page not found.", http.StatusNotFound)
-	}
-}
-
-
-/* NOTES:
-	http.Handler -> interface with the ServeHTTP method
-	http.HandlerFunc -> a function type that accepts same args as ServeHTTP method. Also implements http.Handler
-
-	http.Handle("/", http.Handler)
-	http.HandleFunc("/", pathHandler)
- */
-
-/* FOR EXAMPLE PURPOSE
-	type Server struct{
-		DB string
-	}
-
-	func (s *Server) HomeHandler(w http.ResponseWriter, r *http.Request){
-
-	}
-*/
-
-
 func main() {
-	var router Router
-
-	//http.HandleFunc("/", pathHandler)
-	//http.HandleFunc("/", homeHandler)
-	//http.HandleFunc("/contact", contactHandler)
-
-	/* FOR EXAMPLE PURPOSE
-		var s Server
-		http.HandleFunc("/", s.HomeHandler )
-		http.Handle("/", http.HandlerFunc(contactHandler))
-
-	 */
+	r := chi.NewRouter()
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.Get("/faq", faqHandler)
+	
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Page not found.", http.StatusNotFound)
+	})
 
 	fmt.Println("Starting server on :4500....")
-	http.ListenAndServe(":4500", router)
+	http.ListenAndServe(":4500", r)
 }
+
